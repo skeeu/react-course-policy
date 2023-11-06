7588bc0771789abdba4dec0be96bbfe15c04c40f
skeeu <bisembaev.arman@gmail.com>
2023-11-06 15:11:19 +0600

    common policy for courses

--- src/app/greeting/page.js
+++ src/app/greeting/page.js
@@ -1,26 +1,32 @@
-'use client'
+'use client';
 
-import Page from "@/components/Page";
-import { Heading, ListItem, OrderedList, VStack } from "@chakra-ui/react";
+import Page from '@/components/Page';
+import { Heading, ListItem, OrderedList, VStack } from '@chakra-ui/react';
 
 const questions = [
     'Как вас зовут?',
     'Чем занимаетесь внеурочное время?',
     'Почему решили заниматься веб-разработкой?',
-    'Какие есть знания о React?',
-    'Какие ожидания от курса?'
-]
+    'Какие есть знания о курсе?',
+    'Какие ожидания от курса?',
+];
 
 export default function Greeting() {
     return (
         <Page>
             <VStack>
-                <Heading size='md' textAlign='center'>Давайте ответим на несколько вопросов и познакомимся</Heading>
+                <Heading
+                    size="md"
+                    textAlign="center"
+                >
+                    Давайте ответим на несколько вопросов и познакомимся
+                </Heading>
                 <OrderedList>
-                    {questions.map((q, i) => <ListItem key={i}>{q}</ListItem>)}
+                    {questions.map((q, i) => (
+                        <ListItem key={i}>{q}</ListItem>
+                    ))}
                 </OrderedList>
             </VStack>
-
         </Page>
-    )
-} 
+    );
+}
